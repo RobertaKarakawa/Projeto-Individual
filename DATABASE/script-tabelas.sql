93bf6fc41e875d27faed418830c24171de4b7436
RobertaKarakawa <roberta.karakawa@sptech.school>
2026-05-12 15:45:07 -0300

    add script de games

--- DATABASE/script-tabelas.sql
+++ DATABASE/script-tabelas.sql
@@ -9,3 +9,48 @@ senha VARCHAR(255) NOT NULL,
 dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+
+CREATE TABLE jogo (
+    idJogo INT PRIMARY KEY AUTO_INCREMENT,
+    nomeJogo VARCHAR(100) NOT NULL,
+    genero VARCHAR(50)
+);
+
+CREATE TABLE biblioteca (
+    idBiblioteca INT AUTO_INCREMENT,
+    fkUsuario INT,
+    fkJogo INT,
+    favorito BOOLEAN DEFAULT FALSE,
+    statusJogo VARCHAR(20),
+    nota INT,
+    dataAdicao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pkComposta
+        PRIMARY KEY (idBiblioteca, fkUsuario, fkJogo),
+    CONSTRAINT fkUsuarioBiblioteca
+        FOREIGN KEY (fkUsuario)
+        REFERENCES usuario(idUsuario),
+    CONSTRAINT fkJogoBiblioteca
+        FOREIGN KEY (fkJogo)
+        REFERENCES jogo(idJogo),
+    CONSTRAINT chkStatus
+        CHECK (statusJogo IN (
+                'Não iniciado',
+                'Jogando',
+                'Zerado',
+                'Platinado')),
+    CONSTRAINT chkNota
+        CHECK (nota BETWEEN 1 AND 5)
+);
+
+SELECT j.nomeJogo, COUNT(*) AS totalFavoritos
+FROM biblioteca b
+JOIN jogo j ON b.fkJogo = j.idJogo
+WHERE favorito = TRUE
+GROUP BY j.nomeJogo
+ORDER BY totalFavoritos DESC;
+
+
+SELECT j.nomeJogo, AVG(b.nota) AS mediaNotas
+FROM biblioteca b
+JOIN jogo j ON b.fkJogo = j.idJogo
+GROUP BY j.nomeJogo;
